cc64e7faa07fc16b4b77373204b9e3bd7d8ca505
Rotaru Sergey <rsv@arenadata.io>
2020-02-11 16:23:27 +0300

    Fix tests Removed too broad blob expression that excluded needed files from test bundle

--- adcm_client/packer/add_to_tar.py
+++ adcm_client/packer/add_to_tar.py
@@ -26,7 +26,7 @@ def compare_helper(version, except_list):
     if version is None:
         except_list.extend([
             'spec.yaml', 'pylintrc', '.[0-9a-zA-Z]*', '*pycache*',
-            'README.md', '*test*', '*requirements*', '*.gz', '*.md'])
+            'README.md', '*requirements*', '*.gz', '*.md'])
 
         def v_None(sub: DirEntry):
             for n in except_list:
