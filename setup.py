186dd1dd48d1494b329edbbc42e2bdd8ecb6b1ee
Alexandr Alferov <aas@arenadata.io>
2020-04-27 12:45:48 +0300

    ADCM-1174 Bump version in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extras = {
 
 setuptools.setup(
     name="adcm_client",
-    version="2020.03.30.18",
+    version="2020.04.20.19",
     author="Anton Chevychalov",
     author_email="cab@arenadata.io",
     description="ArenaData Cluster Manager Client",
