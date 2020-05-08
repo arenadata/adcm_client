ffac5fb4974b9772048499896c083430fd062a3a
Rotaru Sergey <rsv@arenadata.io>
2020-05-08 16:22:29 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extras = {
 
 setuptools.setup(
     name="adcm_client",
-    version="2020.05.06.16",
+    version="2020.05.08.16",
     author="Anton Chevychalov",
     author_email="cab@arenadata.io",
     description="ArenaData Cluster Manager Client",
