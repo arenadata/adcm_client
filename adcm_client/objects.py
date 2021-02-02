8a8957801d9a11c6c824be5f7e227f80f8062663
Alexandr Alferov <aas@arenadata.io>
2021-02-02 10:36:13 +0300

    ADCM-1374 Fixed disabling pylint warnings

--- adcm_client/objects.py
+++ adcm_client/objects.py
@@ -9,7 +9,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# pylint: disable=R0901, R0904, W0401, C0302, E0202, E1135, E1136
+# pylint: disable=too-many-lines, too-many-public-methods, too-many-ancestors
 
 import logging
 from collections import abc
