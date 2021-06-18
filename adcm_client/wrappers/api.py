3a4cc5af4dd31c6933fbeaae92020262abb7b0da
Dmitry Chernukhin <madlabman@mail.ru>
2021-06-18 16:29:22 +0400

    Reload client on the fly
    Usefull in upgrade tests.

--- adcm_client/wrappers/api.py
+++ adcm_client/wrappers/api.py
@@ -106,7 +106,7 @@ class ADCMApiWrapper():
                 raise ADCMApiError(data['code'], data['desc'])
 
     def auth(self, username, password):
-        """Auth api client in ADCM and get schema"""
+        """Auth api client in ADCM and fetch schema"""
 
         result = requests.request(
             'POST', self.url + '/api/v1/token/',
@@ -117,10 +117,15 @@ class ADCMApiWrapper():
             scheme='Token',
             token=token['token']
         )
+        self.api_token = token['token']
         self.client = coreapi.Client(auth=auth)
+        self.fetch()
+
+    def fetch(self):
+        """Fetch objects"""
+
         self.schema = self.client.get("{}{}schema/".format(self.url, self.api_url))
         self.objects = self._parse_schema(self.schema, is_allure=IS_ALLURE)
-        self.api_token = token['token']
         try:
             self.adcm_version = self.objects.info.list()['adcm_version']
         except (KeyError, AttributeError):
